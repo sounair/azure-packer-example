e74c93ee0700e98392130c2641a417ab3a1de2e0
soumya <sounair@gmail.com>
2022-02-10 13:03:56 +0530

    test123

--- bin/install.sh
+++ bin/install.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
-apt-get update
-apt-get upgrade -y
-apt-get -y install nginx
-/usr/sbin/waagent -force -deprovision+user && export HISTSIZE=0 && sync
-MYNAME="SOUMYA"
-echo "My name is ${MYNAME}"
+apt update
+apt install git
+apt install wget
+wget https://dl.google.com/go/go1.16.4.linux-amd64.tar.gz
+sudo tar -xvf go1.16.4.linux-amd64.tar.gz
+sudo mv go /usr/local 
+export GOROOT=/usr/local/go
+export GOPATH=$HOME/Projects/Proj1 
+export PATH=$GOPATH/bin:$GOROOT/bin:$PATH 
+sudo cd ~/Projects/Proj1
+git clone "https://github.com/skanayi/golang-hello-world.git"
+cd golang-hello-world/
+git checkout 1.0.3
+go build
+./ -port 81
