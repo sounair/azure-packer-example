1a18da8b225232b6985457f786287089a33ad35c
soumya <sounair@gmail.com>
2022-02-12 17:40:17 +0530

    added sudo again

--- packer_setup.sh
+++ packer_setup.sh
@@ -1,3 +1,3 @@
-curl -fsSL https://apt.releases.hashicorp.com/gpg |apt-key add -
+curl -fsSL https://apt.releases.hashicorp.com/gpg |sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 sudo apt-get update -y && sudo apt-get install packer -y
